294086355513ab8c67da266877c5a85b784ab062
Juan Manuel Maroñas <jmmaronas@gmail.com>
2023-03-24 18:18:07 -0300

    fin de ejercicio

---
+++ src/com/ejercicio1/ImprimirDatos.java
@@ -0,0 +1,18 @@
+package com.ejercicio1;
+
+public class ImprimirDatos {
+    public static void main(String[] args) {
+        int numero = 1;
+        double decimal = 1.2;
+        long numeroG = 125456;
+        String palabra = "Hello World";
+        boolean verdadero = true;
+
+        System.out.println(numero);
+        System.out.println(decimal);
+        System.out.println(numeroG);
+        System.out.println(palabra);
+        System.out.println(verdadero);
+
+    }
+}
